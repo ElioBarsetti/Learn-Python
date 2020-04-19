467de1bf11934a19c6bf2857b0ebcb4e6b5a4ec3
ElioB-PC <elio.barsetti@gmail.com>
2020-04-19 13:50:15 -0400

    Building a Multiple Choice Quiz (3h 57min 39sec)

--- Python Course Beginners.py
+++ Python Course Beginners.py
@@ -447,28 +447,32 @@ print("\n")
 
 
 #----- Building a Multiple Choice Quiz (3h 57min 39sec)  -----
-from Question import Question
-question_prompts = [
-    "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
-    "What color are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
-    "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
-]
+# from Question import Question
+#
+# question_prompts = [
+#     "What color are apples?\n(a) Red/Green\n(b) Purple\n(c) Orange\n\n",
+#     "What color are bananas?\n(a) Teal\n(b) Magenta\n(c) Yellow\n\n",
+#     "What color are strawberries?\n(a) Yellow\n(b) Red\n(c) Blue\n\n"
+# ]
+#
+#
+# questions = [
+#     Question(question_prompts[0], "a"),
+#     Question(question_prompts[1], "c"),
+#     Question(question_prompts[2], "b"),
+# ]
+#
+# def run_test(questions):
+#     score = 0
+#     for question in questions:
+#         answer = input(question.prompt)
+#         if answer == question.answer:
+#             score += 1
+#     print("You got " + str(score) + "/" + str(len(questions)) + " correct.")
+#
+# run_test(questions)
 
-questions = [
-    Question(question_prompts[0], "a"),
-    Question(question_prompts[1], "c"),
-    Question(question_prompts[2], "b"),
-]
 
-def run_test(questions):
-    score = 0
-    for question in questions:
-        answer = input(question_prompts)
-        if answer == question.answer:
-            score += 1
-    print("You got " + str(score) + "/" + str(len(questions)) + " correct.")
-
-run_test(questions)
-
+#----- Object Functions (4h 08min 28sec)  -----
 
 
